89e64ac1fb83ff847714fd4be3c696b9465d7eca
Jeanne-Kamikaze <jeannekamikaze@gmail.com>
2013-02-17 21:42:39 +0100

    Testing pfm loading

--- c/main.cc
+++ c/main.cc
@@ -10,9 +10,7 @@ int write_pgm (const char* path, float* data, int w, int h)
     const int n = w*h;
     int i;
     FILE* f = fopen (path, "w");
-    if (!f) {
-        return 0;
-    }
+    if (!f) return 0;
     fprintf (f, "P5 %d %d %d\n", w, h, 255);
     for (i = 0; i < n; ++i, ++data) {
         unsigned char c = (char) (*data * 255.0f);
@@ -20,6 +18,26 @@ int write_pgm (const char* path, float* data, int w, int h)
     }
     fclose (f);
     return 1;
+}
+
+int write_pfm (const char* path, float* data, int w, int h)
+{
+    const int n = w*h;
+    int i;
+    FILE* f = fopen (path, "w");
+    if (!f) return 0;
+    fprintf (f, "Pf %d %d %f\n", w, h, -1.0f);
+    fwrite (data, sizeof(float), n, f);
+    fclose (f);
+    return 1;
+}
+
+const char* get_extension (const char* path)
+{
+    size_t n = strlen (path);
+    const char* p = path + n - 1;
+    while (p != path && *p != '.') --p;
+    return ++p;
 }
 
 void barf (const char* err)
@@ -50,15 +68,18 @@ int main (int argc, char** argv)
 
     if (argc < 2 || strcmp(argv[1], "--help") == 0) {
         fprintf (stderr, "Usage: %s <output file> [options]\n", argv[0]);
+        fprintf (stderr, "\n");
+        fprintf (stderr, "Output file must be .pgm or .pfm\n");
+        fprintf (stderr, "\n");
         fprintf (stderr, "Options:\n");
         fprintf (stderr, "    -w <int>                            - Output image width\n");
         fprintf (stderr, "    -h <int>                            - Output image height\n");
         fprintf (stderr, "    -s <int>                            - Random seed\n");
+        fprintf (stderr, "    -c <cell size>                      - Size of a cell in the lattice\n");
         fprintf (stderr, "    -t <perlin | fBm> [noise options]   - Noise type\n");
         fprintf (stderr, "\n");
         fprintf (stderr, "Perlin options:\n");
         fprintf (stderr, "    -f <freq>                           - Frequency\n");
-        fprintf (stderr, "    -c <cell size>                      - Size of a cell in the lattice\n");
         fprintf (stderr, "\n");
         fprintf (stderr, "fBm options:\n");
         fprintf (stderr, "    -n <int>                            - Number of octaves to add\n");
@@ -99,10 +120,22 @@ int main (int argc, char** argv)
         printf ("Running fBm\n");
         fbm (seed, bits, w, h, cell_size, lacunarity, H, octaves);
     }
-    if (!write_pgm (f_out, bits, w, h)) {
-        fprintf (stderr, "Failed writing to %s.\n", f_out);
+    const char* ext = get_extension (f_out);
+    int result = 0;
+    if      (strcmp (ext, "pgm") == 0) result = write_pgm (f_out, bits, w, h);
+    else if (strcmp (ext, "pfm") == 0) result = write_pfm (f_out, bits, w, h);
+    else {
+        fprintf (stderr, "Unsupported file format: %s\n", ext);
+        return -1;
     }
-    printf ("Image successfully written to %s\n", f_out);
+    if (!result) {
+        fprintf (stderr, "Failed writing to %s\n", f_out);
+    }
+    else printf ("Image successfully written to %s\n", f_out);
+    for (int i = 0; i < 4; ++i) printf ("%f, ", bits[i]);
+    printf ("\n");
+    for (int i = w*h-1; i >= w*h-4; --i) printf ("%f, ", bits[i]);
+    printf ("\n");
     delete[] bits;
 
     return 0;
